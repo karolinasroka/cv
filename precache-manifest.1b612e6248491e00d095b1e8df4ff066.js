faa2989eb8a621062f784488aa9057405185be46
Karolina Sroka <karolinasroka@users.noreply.github.com>
2020-06-17 01:50:45 +0200

    Updates

---
+++ precache-manifest.1b612e6248491e00d095b1e8df4ff066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1251af6699f56905bdd89e55fc746a72",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "aa93bd06f38f4d924bf4",
+    "url": "/cv/static/css/main.e5bb368e.chunk.css"
+  },
+  {
+    "revision": "4c5f811ce338c70d4fa3",
+    "url": "/cv/static/js/2.652704da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.652704da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa93bd06f38f4d924bf4",
+    "url": "/cv/static/js/main.ff3a9381.chunk.js"
+  },
+  {
+    "revision": "d6dcef095c971930b9da",
+    "url": "/cv/static/js/runtime-main.e4fe9350.js"
+  },
+  {
+    "revision": "9ba8baf29d82ffd012c7e1d689d14c5e",
+    "url": "/cv/static/media/img.9ba8baf2.jpg"
+  }
+]);
